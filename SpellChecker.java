365062c360258076080ab5690e23b02b2a1c3714
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2024-01-29 17:47:35 +0200

    test1

--- SpellChecker.java
+++ SpellChecker.java
@@ -8,28 +8,128 @@ public class SpellChecker {
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
+
+		
 	}
 
 	public static String tail(String str) {
 		// Your code goes here
+		return str.substring(1,str.length());
+
 	}
+
+	
+
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		if (word2.length() == 0) 
+		{
+			return word1.length();	
+		}
+		if (word1.length() == 0) 
+		{
+			return word2.length();	
+		}
+		if (word1.charAt(0) == word2.charAt(0)) 
+		{
+			return levenshtein(tail(word1),tail(word2));	
+		}
+		else
+		{
+			return Math.min((levenshtein(tail(word1),word2)),Math.min((levenshtein(word1,tail(word2))),levenshtein(tail(word1),tail(word2)))) + 1;
+
+
+		}
 	}
+
+	
+
+
+
+
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0;i < 3000 ;i++ ) 
+		{
+			dictionary[i] = in.readLine();	
+		}
 
 		return dictionary;
 	}
 
+	
+
+
+
+
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		int [] editDistance = new int[3000];
+		for (int i = 0;i < 3000 ;i++) 
+		{
+			editDistance[i] = levenshtein(word,dictionary[i]);	
+		}
+		int minEditDistance = minOfArray(editDistance);
+		int indexOfMin = indexOfMinInArray(editDistance);
+		if (minEditDistance <= threshold) 
+		{
+			return dictionary[indexOfMin];	
+		}
+		else
+		{
+			return word;
+		}
+
+	}
+
+
+	public static int minOfArray(int [] array)
+	{
+		int min = array[0];
+		for (int i = 1;i < array.length ;i++) 
+		{
+			if (array[i] < min) 
+			{
+				min = array[i];	
+					
+			}	
+		}
+		return min;
+	}
+	public static int indexOfMinInArray(int [] array)
+	{
+		int index = 0;
+		for (int i = 0;i < array.length ;i++) 
+		{
+			if (array[i] == minOfArray(array)) 
+			{
+				index = i;	
+				break;
+			}
+		}
+		return index;
+
+	}
+	public static void print(int [] array)
+	{
+		for (int i = 0;i < array.length ;i++) 
+		{
+			System.out.println(array[i]);	
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
